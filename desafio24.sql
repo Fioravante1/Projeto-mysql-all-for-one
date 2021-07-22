20a40a463be62b9a186f9f67d67b677edd9cc731
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-07-22 19:24:23 -0300

    Refactor: Inclui um zero

--- desafio24.sql
+++ desafio24.sql
@@ -3,6 +3,6 @@ UPDATE
 SET
   discount = 45
 WHERE
-  unit_price > 10.000
+  unit_price > 10.0000
   AND id BETWEEN 30
   AND 40;
--- desafio25.sql
+++ desafio25.sql
@@ -1,4 +1,4 @@
 DELETE FROM
   order_details
 WHERE
-  unit_price < 10.000;
+  unit_price < 10.0000;
